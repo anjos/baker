b83fb1e579d023bcd2ec70897fc9b11237f4c4eb
Andre Anjos <andre.dos.anjos@gmail.com>
2018-03-28 11:02:43 +0200

    Add tests to B2 backend

--- baker/test_b2.py
+++ baker/test_b2.py
@@ -83,11 +83,83 @@ def test_restic_init():
 
   # Cleans-up bucket before starting
   b2.empty_bucket(TEST_BUCKET_NAME)
-
-  out = restic.init('b2:%s' % TEST_BUCKET_NAME, [], 'password')
+  repo = 'b2:%s' % TEST_BUCKET_NAME
+  out = restic.init(repo, [], 'password')
 
   messages = out.split('\n')[:-1] #removes last end-of-line
   nose.tools.eq_(len(messages), 5)
   assert messages[0].startswith('created restic repository')
   assert messages[0].endswith(TEST_BUCKET_NAME)
   assert 'config' in b2.bucket_contents(TEST_BUCKET_NAME)
+
+
+def test_restic_backup():
+
+  b2.empty_bucket(TEST_BUCKET_NAME)
+  repo = 'b2:%s' % TEST_BUCKET_NAME
+  restic.init(repo, [], 'password')
+  out = restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+
+  messages = out.split('\n')[:-1] #removes last end-of-line
+  nose.tools.eq_(len(messages), 7)
+  assert messages[0] == ('scan [%s]' % SAMPLE_DIR)
+  assert messages[-1].startswith('snapshot')
+  assert messages[-1].endswith('saved')
+
+
+def test_restic_check():
+
+  b2.empty_bucket(TEST_BUCKET_NAME)
+  repo = 'b2:%s' % TEST_BUCKET_NAME
+  restic.init(repo, [], 'password')
+  restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+  out = restic.check(repo, [], 'password')
+
+  messages = out.split('\n')[:-1] #removes last end-of-line
+  nose.tools.eq_(len(messages), 5)
+  nose.tools.eq_(messages[0], 'create exclusive lock for repository')
+  nose.tools.eq_(messages[1], 'load indexes')
+  nose.tools.eq_(messages[-1], 'no errors were found')
+
+
+def test_restic_snapshots():
+
+  b2.empty_bucket(TEST_BUCKET_NAME)
+  repo = 'b2:%s' % TEST_BUCKET_NAME
+  restic.init(repo, [], 'password')
+  restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+  restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+  data = restic.snapshots(repo, ['--json'], 'hostname', 'password')
+
+  # data is a list of dictionaries with the following fields
+  #   * time: The time the snapshot was taken (as a datetime.datetime obj)
+  #   * tree: The snapshot tree identifier?
+  #   * paths: List of paths backed-up on that snapshot
+  #   * hostname: The name of the host
+  #   * username: The user identifier of the user that made the snapshot
+  #   * uid: The user id for the snapshot file
+  #   * gid: The groud id for snapshot file
+  #   * id: The snapshot identifier
+  #   * short_id: A shortened version of ``id``
+  nose.tools.eq_(len(data), 2)
+  nose.tools.eq_(data[0]['paths'], [SAMPLE_DIR])
+  nose.tools.eq_(data[0]['hostname'], 'hostname')
+  nose.tools.eq_(data[1]['paths'], [SAMPLE_DIR])
+  nose.tools.eq_(data[1]['hostname'], 'hostname')
+
+
+def test_restic_forget():
+
+  b2.empty_bucket(TEST_BUCKET_NAME)
+  repo = 'b2:%s' % TEST_BUCKET_NAME
+  restic.init(repo, [], 'password')
+  restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+  data1 = restic.snapshots(repo, ['--json'], 'hostname', 'password')
+  restic.backup(SAMPLE_DIR, repo, [], 'hostname', [], 'password')
+  restic.forget(repo, [], 'hostname', True, {'last': 1}, 'password')
+  data2 = restic.snapshots(repo, ['--json'], 'hostname', 'password')
+
+  # there are 2 backups which are nearly identical
+  nose.tools.eq_(len(data2), 1)
+  nose.tools.eq_(data2[0]['parent'], data1[0]['id'])
+  assert data2[0]['id'] != data1[0]['id']
