0b6609082c870e70116928c64b5996f00669ae42
Andre Anjos <andre.dos.anjos@gmail.com>
2018-04-07 12:24:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='baker',
-    version='1.2.0',
+    version='1.2.1',
     description="Utilities for backing-up my QNAP on BackBlaze's B2",
     url='https://github.com/anjos/baker',
     license="GPLv3",
