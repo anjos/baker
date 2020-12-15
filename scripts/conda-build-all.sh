0e8f443de9094383e6ec87a5f8528c33d7b945c6
Andre Anjos <andre.dos.anjos@gmail.com>
2020-12-15 09:38:49 +0100

    [deps,scripts] Updated all dependencies

--- scripts/conda-build-all.sh
+++ scripts/conda-build-all.sh
@@ -4,32 +4,41 @@
 script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 
 simple_pkgs=()
-#simple_pkgs+=('deps/restic')
+simple_pkgs+=('deps/restic')
 
 python_versions=()
-python_versions+=('3.7')
+python_versions+=('3.8')
 
 python_pkgs=()
 python_pkgs+=('conda') #baker itself
 
 noarch_pkgs=()
-#noarch_pkgs+=('deps/logfury')
-#noarch_pkgs+=('deps/schedule')
-#noarch_pkgs+=('deps/b2sdk')
-#noarch_pkgs+=('deps/b2')
+noarch_pkgs+=('deps/logfury')
+noarch_pkgs+=('deps/phx-class-registry')
+noarch_pkgs+=('deps/schedule')
+noarch_pkgs+=('deps/b2sdk')
+noarch_pkgs+=('deps/b2')
+
+echo -e "#!/usr/bin/env bash\n"
 
 for p in "${simple_pkgs[@]}"; do
-  #conda build ${p}
-  ${script_dir}/conda-build-docker.sh /work/$p
-done
-
-for p in "${noarch_pkgs[@]}"; do
-  conda build --no-test $p
+  echo "## To build '${p}', issue the following:"
+  echo conda build -c anjos ${p}
+  echo ${script_dir}/conda-build-docker.sh /work/$p
+  echo ""
 done
 
 for pyver in "${python_versions[@]}"; do
   for p in "${python_pkgs[@]}"; do
-    conda build --no-test --python=$pyver $p
-    ${script_dir}/conda-build-docker.sh --no-test --python=$pyver /work/$p
+    echo "## To build '${p}', issue the following:"
+    echo conda build -c anjos --python=$pyver $p
+    echo ${script_dir}/conda-build-docker.sh --python=$pyver /work/$p
+    echo ""
   done
+done
+
+for p in "${noarch_pkgs[@]}"; do
+  echo "## To build '${p}', issue the following:"
+  echo conda build -c anjos $p
+  echo ""
 done
